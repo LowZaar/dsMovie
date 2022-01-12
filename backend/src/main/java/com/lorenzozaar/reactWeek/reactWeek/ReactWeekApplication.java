73fde04ec791cda7b33a9ff4f651076fc09316a0
Low Zaar <watchmefail12@gmail.com>
2022-01-12 19:46:18 -0300

    Project Created

---
+++ backend/src/main/java/com/lorenzozaar/reactWeek/reactWeek/ReactWeekApplication.java
@@ -0,0 +1,13 @@
+package com.lorenzozaar.reactWeek.reactWeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactWeekApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lorenzozaar/reactWeek/reactWeek/ReactWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenzozaar.reactWeek.reactWeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
