ec4938d7e90fbff9ec1902236973079e221c306f
Low Zaar <watchmefail12@gmail.com>
2022-01-16 21:00:55 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,13 +1,26 @@
 import axios from "axios"
 import MovieCard from "components/movieCard"
 import Pagination from "components/pagination"
+import { useEffect, useState } from "react"
 import { BASE_URL } from "utils/requests"
+import { MoviePage } from "types/movie"
 function Listing() {
+    
+    const [pageNumber, setPageNumber] = useState(0);
+    
+    useEffect(() => {
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(obj => {
-        console.log(obj.data)
-    })
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(obj => {
+            const data = obj.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number)
+        });
+
+    }, []);
+
+
+
 
     return (
         <>  
